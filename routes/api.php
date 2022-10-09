065573b40e6d79d081807278b9cc5f3f47ff7c88
Juan Manuel Guerrero Cañon <juanmanuel@bellpi.com>
2022-10-09 17:14:41 -0500

    passport-implementation

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Passport Keys
+//Encryption keys generated successfully.
+//Personal access client created successfully.
+//Client ID: 1
+//Client secret: R6u7qx4KYmdCCC2iXBLv4253Ab9UV9c67QpKLbqQ
+//Password grant client created successfully.
+//Client ID: 2
+//Client secret: DY8JJy5JvvWPMWS744YtZpQhsuxoOT3oUkuoZksX
